99ddb4b34f5e16bd3f8e9c172c1ae913da36c040
Brigitta <bridgetlandia@gmail.com>
2020-07-24 16:11:44 +0200

    Adding category and article queries

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import Articles from '../Articles';
 import Nav from '../../components/Nav';
+import Article from '../Article';
 
 function App() {
 	return (
 		<div className="App">
 			<Nav />
-			<Articles />
+			<Switch>
+				<Route exact path="/" component={Articles} />
+				<Route exact path="/article/:id" component={Article} />
+			</Switch>
 		</div>
 	);
 }
